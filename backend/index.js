95747b64fa8f86692cdd134623debf792f55bd03
Franco Segovia <fran.segovia545@gmail.com>
2026-06-19 22:08:25 -0300

    Importamos apis en index, niciamos servidor y creamos archivos vistas en front

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,54 @@
-hola
+/////////////////////
+// Importaciones
+import express from "express";
+import environment from "./src/api/config/environment.js"; // importamos los datos de la BD
+import connection from "./src/api/database/db.js"; // importamos pool de conexiones
+import cors from "cors"; // da permiso a las solicitudes / peticiones
+
+const app = express();
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/////////////////////
+// Config
+const PORT = environment.port; // puerto de la base de datos
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/////////////////////
+// Middlewares
+
+// Middleware basico para permitir todas las solicitudes
+app.use(cors()); 
+
+// Middleware logger para analizar todas las solicitudes por consola (tener el historial del consumo de nuestra Api REST en la consola)
+app.use((req, res, next) => {
+    let fecha = new Date();
+    console.log(`[${fecha.toLocaleDateString()} ${fecha.toLocaleTimeString()}] ${req.method} ${req.url}`);
+    
+    next(); // next() da paso a que continue la respuesta o el siguiente middleware (en caso de haberlo)
+});
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/////////////////////
+// Endpoints
+
+app.get("/", (req, res) => {
+    res.send("Libreria Dominico");
+});
+
+// endpoint GET (L)
+
+// endpoint GET BY ID (L)
+
+// endpoint POST (F)
+
+// endpoint PUT (F)
+
+// endpoint DELETE (F)
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
--- backend/src/api/database/db.js
+++ backend/src/api/database/db.js
@@ -2,10 +2,10 @@
 import mysql2 from "mysql2/promise"
 
 // Importamos la informacion de la conexion a la BBDD
-import environments from "../environments.js"
+import environment from "../config/environment.js"
 
 // Extraemos solo el objeto DataBase
-const {database} = environments
+const {database} = environment
 
 // Creamos la conexion a la BBDD (un pool de conexiones)
 const connection = mysql2.createPool({
@@ -16,5 +16,14 @@ const connection = mysql2.createPool({
 })
 
 export default connection;
-// sql 2 es e modulo que nos prodvee metodos para conectarnos a la BBDD 
-// createPool( es una funcion que crea un grupo (pool) de conexiones a la BBDD)
+
+/* 
+    - mysql2 es el modulo que nos provee de metodos para conectarnos a la BBDD
+
+    - createPool() es una funcion que crea un grupo (pool) de conexiones a la BBDD
+        - Crea un gestor de conexiones automatico
+        - Se conecta a la BBDD usando los parametros (host, user, password, etc)
+        - Por defecto abre hasta 10 conexiones simultaneas
+        - Ya que importarmos mysql2/promises permite usar await connection.query() para ejecutar SQL
+        - Le pasamos la configuracion desde el objeto databse
+*/
