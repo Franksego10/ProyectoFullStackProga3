9f5401e0de28555b22fddc10deca42facf157fba
Franco Segovia <fran.segovia545@gmail.com>
2026-07-01 15:29:59 -0300

    Creamos variables de colores en el css y agregamos boton funcion para cambiar de tema claro/oscuro en el dashboard

--- backend/src/public/js/header.js
+++ backend/src/public/js/header.js
@@ -9,3 +9,32 @@ btnMenu.addEventListener("click", () => {
         menu.classList.add("mostrar");
     }
 });
+
+const btnTema = document.getElementById("btn-tema");
+const iconoTema = document.getElementById("icono-dark");
+
+btnTema.addEventListener("click", () => {
+    // cambiamos la clase de html a modo oscuro, si ya lo tiene se lo quita
+    document.documentElement.classList.toggle("dark-mode");
+    // preguntamos si html contiene la class dark mode
+    const esDark = document.documentElement.classList.contains('dark-mode');
+    // si tiene lo guarda en "tema" el valor "dark" en el local storage, sino "light"
+     if (esDark){
+         // textContent interpreta texto plano, innerHTML contenido html
+         iconoTema.textContent = "☀️";
+         // se fija el ultimo hijo
+         btnTema.lastChild.textContent = "MODO CLARO";
+     }else{
+         // textContent interpreta texto plano, innerHTML contenido html
+         iconoTema.textContent = "🌙";
+         // se fija el ultimo hijo
+         btnTema.lastChild.textContent = "MODO OSCURO";
+     }
+    localStorage.setItem('tema', esDark ? 'dark' : 'light');
+
+})
+    // Al cargar la página, sincroniza el botón con el tema guardado
+    if (localStorage.getItem('tema') === 'dark') {
+        iconoTema.textContent = "☀️";
+        btnTema.lastChild.textContent = "MODO CLARO";
+    }
