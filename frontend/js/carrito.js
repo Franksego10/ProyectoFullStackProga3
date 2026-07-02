d106836b08c770b8f0507342bda8d7e17b080d5a
Franco Segovia <fran.segovia545@gmail.com>
2026-07-02 15:29:44 -0300

    Agregue carpetas css y assets a frontend para trabajarlo como app individual. Ademas implementamos boton para cambiar de tema oscuro a claro y viceversa. Se mejoro diseño responsive

--- frontend/js/carrito.js
+++ frontend/js/carrito.js
@@ -54,8 +54,15 @@ function mostrarCarrito(){
     let carritoProdHTML = `
     <div class='contenedor-botones-carrito'>
         <h2 class="titulo-contenedor-productos">Productos en el carrito</h2>
-        <button class='boton-vaciar' onclick='vaciarCarrito()'>Vaciar carrito</button>
-        <span>Total:<span id="contenedor-totalPrecio">$${totalPrecio}</span> </span>
+        <button class='boton-vaciar' onclick='vaciarCarrito()'>
+            <span class="texto-vaciar">Vaciar carrito</span>
+            <span class="icono-vaciar">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+            </span>
+        </button>
+        <span class="texto-carrito">Total:<span id="contenedor-totalPrecio">$${totalPrecio}</span> </span>
         <button class='boton-comprar' onclick="imprimirTicket()" id="btn-comprar">Comprar</button>
     </div>
     <ul>
@@ -66,7 +73,7 @@ function mostrarCarrito(){
                 <p class="nombre-item">${element.nombre} - $${element.precio}</p>
                 <div class="modificar-cantidad">
                     <button class="boton-sumar" onclick="sumarCantidadProducto(${element.id})" id="${element.id}">+</button>
-                    <span>${element.cantidad}</span>
+                    <span class="texto-carrito">${element.cantidad}</span>
                     <button class="boton-restar" onclick="restarCantidadProducto(${element.id})" id="${element.id}">-</button>
                 </div>
             </li>
